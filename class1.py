798a9b13abdb9723d1b0e698e4dfbd3923caf36a
MPrasad1611 <prasadchowdary1611@gmail.com>
2024-12-21 19:42:03 +0530

    changes

--- class1.py
+++ class1.py
@@ -1,4 +1,11 @@
-a=int(input("Enter a Number To print:"))
-b=int(input("Enter a Number To print:"))
-print(a+b)
-print(type(a))
+# a=int(input("Enter a Number To print:"))
+# b=int(input("Enter a Number To print:"))
+# print(a+b)
+# print(type(a))
+# print(int(7236==7235))
+# cmp1=2+3j
+# cmp2=5+6j
+# print(cmp1+cmp2)
+
+dict={"r1":"ram","2":"helo"}
+print(dict["2"])
