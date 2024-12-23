d8c1d22c35ce1fa1ac6ead3763d1ccb8f43edda4
MPrasad1611 <prasadchowdary1611@gmail.com>
2024-12-23 23:12:23 +0530

    changes

--- control.py
+++ control.py
@@ -1,4 +1,10 @@
-name="Mohan"
-if name == "Mohan":
-    print("Mohan Is Good boy")
-    
+# name="Mohan"
+# if name == "Mohan":
+#     print("Mohan Is Good boy")
+num=int(input("Enter A Number:"))
+if num > 0:
+    print("positve number")
+elif num < 0:
+    print("Negative Number")
+elif num ==0:
+    print("zero")
