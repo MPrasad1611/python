49af11d5b9f449e3744cbebbed850b131365f0a1
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-01-02 23:27:09 +0530

    changes

--- loop.py
+++ loop.py
@@ -3,14 +3,19 @@
 #         print("Even:",i)
 #     else:
 #         print("Odd:",i)
-i=-10
-while i<=20:
-    if i==0:
-        print("Zero",i)
-    elif i%2==0:
-        print("Even",i)
-    elif i%2!=0:
-        print("Odd",i)
+# i=-10
+# while i<=20:
+#     if i==0:
+#         print("Zero",i)
+#     elif i%2==0:
+#         print("Even",i)
+#     elif i%2!=0:
+#         print("Odd",i)
     
-    i=i+1
+#     i=i+1
 
+# u=350
+# if u<=350:
+#     print("Bill:",u*10)
+# else:
+#     print("Bill:",u*20)
