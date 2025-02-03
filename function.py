f2c3b93fdec31adf62258417a3a03d6f0d9b5a98
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-03 18:14:35 +0530

    changes

--- function.py
+++ function.py
@@ -2,11 +2,16 @@
 #     print(r*r*r)
 
 # calc_vol(3)
-def sum(**agrs):
-    print(type(agrs))
-    print(agrs)
-    sum=0
-    for i,j in agrs.items():
-        sum+=j
-    return sum
-print(sum(a=3,b=6,c=4,d=3,e=6))
+# def sum(**agrs):
+#     print(type(agrs))
+#     print(agrs)
+#     sum=0
+#     for i,j in agrs.items():
+#         sum+=j
+#     return sum
+# print(sum(a=3,b=6,c=4,d=3,e=6));
+def pat(n):
+ for i in range(1,n+1):
+    print(i*"* ") 
+
+pat(7)
