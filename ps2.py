d85d34a08ce0fc977a26bdffefa28ec7f438d0c5
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-04 22:45:13 +0530

    changes

--- ps2.py
+++ ps2.py
@@ -1,5 +1,6 @@
 year=int(input("Enter any year: "))
-if year%4==0 and year%100!=0 or year%400==0:
+if (year%4==0 and year%100!=0) or year%400==0:
     print("Leap Year")
 else:
     print("Not a Leap Year")
+#r="Leap Year"if(year%4==0 and year%100!=0) or year%400==0 else "Not a Leap Year"
--- pstask1.py
+++ pstask1.py
@@ -146,4 +146,4 @@ side3=int(input("Enter the third side of triangle: "))
 if side1+side2>side3 and side2+side3>side1 and side1+side3>side2:
     print("Triangle is valid")
 else:
-    print("Triangle is not valid")
+    print("Triangle is not valid") 
