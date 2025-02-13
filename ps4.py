5db4c0af1fd98931f4076d661c660c024ee2e6ca
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-13 22:44:59 +0530

    changes

--- ps4.py
+++ ps4.py
@@ -9,26 +9,32 @@
 # else:    
 #     print("Not Palindrome")
 
-i=int(input("Choose Operation Number:1.square,2.cube,3.Exit:"))
-while True:
-    if i==1:
-        n=int(input("Enter the number:"))
-        print(n**2)
-        i=int(input("Choose Operation Number:1.square,2.cube,3.Exit:"))
-    elif i==2:
-        n=int(input("Enter the number:"))
-        print(n**3)
-        i=int(input("Choose Operation Number:1.square,2.cube,3.Exit:"))
-    elif i==3:
+# i=int(input("Choose Operation Number:1.square,2.cube,3.Exit:"))
+# while True:
+#     if i==1:
+#         n=int(input("Enter the number:"))
+#         print(n**2)
+#         i=int(input("Choose Operation Number:1.square,2.cube,3.Exit:"))
+#     elif i==2:
+#         n=int(input("Enter the number:"))
+#         print(n**3)
+#         i=int(input("Choose Operation Number:1.square,2.cube,3.Exit:"))
+#     elif i==3:
+#         break
+#     else:
+#         print("Invalid Operation")
+#         i=int(input("Choose Operation Number:1.square,2.cube,3.Exit:"))
+    
+num=int(input("Enter the number to check if it us a prime:"))
+if  num==1:
+    print("Not Prime")
+elif num==2:
+    print("Prime")
+else: 
+ for i in range(2,num):
+    if num%i==0:
+        print("Not Prime")
         break
     else:
-        print("Invalid Operation")
-        i=int(input("Choose Operation Number:1.square,2.cube,3.Exit:"))
-    
-    num=int(input("Enter the number to check if it us a prime:"))
-    for i in range(2,num):
-        if num%i==0:
-            print("Not Prime")
-            break
-    else:
         print("Prime")
+        break
