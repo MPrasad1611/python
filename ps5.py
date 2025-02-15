dd93642248476b816f775ddd41046a2deca3bc1b
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-15 23:59:09 +0530

    changes

--- ps5.py
+++ ps5.py
@@ -1,7 +1,10 @@
 n=int(input("Enter the number:"))
 while True:
-    n+=1
+    n-=1
     c=0
+    if n==1:
+        print("Not Prime")
+        break
     for i in range(1,n+1):
         if n%i==0:
             c+=1  
