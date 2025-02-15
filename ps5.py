75d35e1b0d5c63b11a8c5d885a3372373cb86e29
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-16 00:24:06 +0530

    changes

--- ps5.py
+++ ps5.py
@@ -1,4 +1,6 @@
 n=int(input("Enter the number:"))
+temp=n;
+c2=n;
 while True:
     n-=1
     c=0
@@ -13,15 +15,15 @@ while True:
         break  
 
 while True:
-    n+=1
+    temp+=1
     c=0
-    for i in range(1,n+1):
-        if n%i==0:
+    for i in range(1,temp+1):
+        if temp%i==0:
             c+=1  
     if c==2:
-        max_prime=n
+        max_prime=temp
         break
-if n-min_prime>max_prime-n:
+if c2-min_prime>max_prime-c2:
     print(max_prime)
 else:
     print(min_prime)
