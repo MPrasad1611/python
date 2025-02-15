5118a4fe7ed5424d7bfa43670435c4bd28c3e255
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-16 00:09:09 +0530

    changes

--- ps5.py
+++ ps5.py
@@ -1,20 +1,34 @@
-# n=int(input("Enter the number:"))
-# while True:
-#     n-=1
-#     c=0
-#     if n==1:
-#         print("Not Prime")
-#         break
-#     for i in range(1,n+1):
-#         if n%i==0:
-#             c+=1  
-#     if c==2:
-#         print(n)
-#         break  
-year=int(input("Enter the year:"))
+n=int(input("Enter the number:"))
 while True:
-    year+=1
-    if year%4==0 and year%100!=0 or year%400==0:
-        print(year)
+    n-=1
+    c=0
+    if n==1:
+        print("Not Prime")
         break
+    for i in range(1,n+1):
+        if n%i==0:
+            c+=1  
+    if c==2:
+        min_prime=n
+        break  
+
+while True:
+    n+=1
+    c=0
+    for i in range(1,n+1):
+        if n%i==0:
+            c+=1  
+    if c==2:
+        max_prime=n
+        break
+if n-min_prime>max_prime-n:
+    print(max_prime)
+else:
+    print(min_prime)
+# year=int(input("Enter the year:"))
+# while True:
+#     year+=1
+#     if year%4==0 and year%100!=0 or year%400==0:
+#         print(year)
+#         break
    
