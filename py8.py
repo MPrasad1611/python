810ac29e7f01bb50adf3e7e9cc62ae20cc7c8412
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-18 23:07:18 +0530

    changes

--- py8.py
+++ py8.py
@@ -9,15 +9,31 @@
 #         di[i]=1
 # print(di)
 
-list1=["prasad","sai","yaswanth"]
-fr=[]
-for st in list1:
-  di={}
-  for i in st:
-      if i in di:
-        di[i]+=1
-      else:
-        di[i]=1
-  print(di)
-  fr.append({st: di})
-print(fr)
+# list1=["prasad","sai","yaswanth"]
+# fr=[]
+# for st in list1:
+#   di={}
+#   for i in st:
+#       if i in di:
+#         di[i]+=1
+#       else:
+#         di[i]=1
+#   fr.append({st: di})
+# print(fr)
+def max_min_sum(list_p):
+    if len(list_p) == 0:
+        return
+    max = min = list_p[0]
+    sum=0
+    for i in list_p:
+        max=i if i > max else max
+        min=i if i < min else min
+        sum+=i
+    return max, min, sum
+list1=[1,2,3,4,5,6,7,8,9,10]
+max,min,sum=max_min_sum(list1)
+sl=list1[0]
+for i in list1:
+    if i!=max and i>sl:
+        sl=i
+     
