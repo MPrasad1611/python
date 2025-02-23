81e3a93f2830a72cb25ddca823f25c1c38ae3c06
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-23 21:31:09 +0530

    changes

--- ps9.py
+++ ps9.py
@@ -11,3 +11,39 @@
 # for j in list1:
 #     res.append(max(j))
 # print(res)
+list1=[1,2,3,4,5,6,7,8,9,10]
+key=10
+# low=0
+# high=len(list1)-1 
+# flag=False
+# while low<=high:
+#       mid=(low+high)//2
+#       if list1[mid]==key:
+#             flag=True
+#             print(f"Element is found at {mid}")
+#             break;
+#       elif list1[mid]>key:
+#             high=mid-1
+#       else:
+#             low=mid+1
+        
+# if flag==False:
+#     print("Not found")
+
+def bi(list,key):
+    low=0
+    high=len(list1)-1 
+    while low<=high:
+      mid=(low+high)//2
+      if list1[mid]==key:
+            return mid
+      elif list1[mid]>key:
+            high=mid-1
+      else:
+            low=mid+1
+    return -1
+res=bi(list1,key)
+if res!=-1:
+     print(res)
+else:
+     print("Not Found")
