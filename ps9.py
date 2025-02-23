ae452bbf1607fd7268d82e35dd225aed6165ac6c
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-23 20:38:19 +0530

    changes

---
+++ ps9.py
@@ -0,0 +1,13 @@
+# def max(list):
+#     max=0;
+#     for i in list:
+#         if i>max:
+#             max=i
+#     return max   
+
+
+# list1=[[7,2,3],[4,2,5],[6,7,8]]
+# res=[]
+# for j in list1:
+#     res.append(max(j))
+# print(res)
