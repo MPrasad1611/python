c337ea63170b86fe6b905cc2217b8ebd32043c8c
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-23 23:19:36 +0530

    changes

--- ps9.py
+++ ps9.py
@@ -30,20 +30,31 @@ key=10
 # if flag==False:
 #     print("Not found")
 
-def bi(list,key):
-    low=0
-    high=len(list1)-1 
-    while low<=high:
-      mid=(low+high)//2
-      if list1[mid]==key:
-            return mid
-      elif list1[mid]>key:
-            high=mid-1
-      else:
-            low=mid+1
-    return -1
-res=bi(list1,key)
-if res!=-1:
-     print(res)
-else:
-     print("Not Found")
+# def bi(list,key):
+#     low=0
+#     high=len(list1)-1 
+#     while low<=high:
+#       mid=(low+high)//2
+#       if list1[mid]==key:
+#             return mid
+#       elif list1[mid]>key:
+#             high=mid-1
+#       else:
+#             low=mid+1
+#     return -1
+# res=bi(list1,key)
+# if res!=-1:
+#      print(res)
+# else:
+#      print("Not Found")
+list1=[1,2,3,4]
+swap=False
+for j in range(len(list1)-1):
+  for i in range(0,len(list1)-j-1):
+    if list1[i]>list1[i+1]:
+       list1[i],list1[i+1]=list1[i+1],list1[i]
+       swap=True
+  if swap==False:
+    print("Already Sorted")
+    break
+print(list1)
