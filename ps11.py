dc9d26433cf3a13f616c5640c14ed1c510d1acb7
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-25 22:04:53 +0530

    changes

--- ps11.py
+++ ps11.py
@@ -1,5 +1,10 @@
-list1=[20,25,26,2,98,6]
+list1=[20,15,26,2,98,6]
 list2=sorted(list1)
 
-print(list1)
-print(list2)
+# print(list1)
+# print(list2)
+re=[]
+for i in list1:
+    temp=list2.index(i)+1
+    re.append(temp)
+print(re)
