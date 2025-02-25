8f32f16edaf1e55b791b2b2f8943d5f5ef47f1a8
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-25 22:29:41 +0530

    changes

--- ps11.py
+++ ps11.py
@@ -1,11 +1,21 @@
-list1=[20,15,26,2,98,6]
-list2=sorted(list1)
+# list1=[20,15,26,2,98,6]
+# list2=sorted(list1)
 
 # print(list1)
 # print(list2)
-re=[]
-for i in list1:
-    temp=(len(list2)-(list2.index(i)))
-    re.append(temp)
-print(re)
-print(list2)
+# re=[]
+# for i in list1:
+#     temp=(len(list2)-(list2.index(i)))
+#     re.append(temp)
+# print(re)
+# print(list2)
+# list=[8,7,1,6,5,9]
+# list.sort();
+# low=len(list)//2
+# high=len(list)-1
+# while low<high:
+#     list[low],list[high]=list[high],list[low]
+#     low+=1
+#     high-=1
+# print(list) 
+list1=[[3,4,5],[6,]7,8,[-10,-11,-12]]
