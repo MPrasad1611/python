1db076c1f9aca8deff4bbd76014feb20c19f31f4
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-25 21:38:16 +0530

    changes

--- ps10.py
+++ ps10.py
@@ -5,7 +5,7 @@
 
 # list=rev
 # print(list)
-list1=[1,2,3,4,5,6,7,8,9,9,9,10,9]
+#list1=[1,2,3,4,5,6,7,8,9,9,9,10,9]
 # low=0
 # high=len(list)-1
 # while low<high:
@@ -14,14 +14,47 @@ list1=[1,2,3,4,5,6,7,8,9,9,9,10,9]
 #     high-=1
 
 # print(list)
-dupl=set()
-uni=set()
-for i in list1:
-    if i in uni:
-        dupl.add(i)
-        uni.remove(i)
-    elif i not in dupl:
-        uni.add(i)
+# dupl=set()
+# uni=set()
+# for i in list1:
+#     if i in uni:
+#         dupl.add(i)
+#         uni.remove(i)
+#     elif i not in dupl:
+#         uni.add(i)
     
-print(list(uni))
-print(list(dupl))
+# print(list(uni))
+# print(list(dupl))
+
+
+
+# def dupli(num1):
+#     cur=[]
+#     while num1>0:
+#         res=num1%10
+#         if res in cur:
+#             return True
+#         cur.append(res)
+#         num1=num1//10
+#         cur.append(res)
+#     return False
+# list1=[898,654,323,666]
+# res=[]
+# for i in list1:
+#     res.append(dupli(i))
+# print(res)
+
+def missing(num1):
+    rem=[]
+    str1=""
+    while num1>0:
+        re=num1%10
+        # if re not in rem:
+        rem.append(re)
+        num1=num1//10
+    for i in range(1,max(rem)):
+        if i not in rem:
+            str1+=str(i)
+    return str1+" missing"
+
+print(missing(34571))
---
+++ ps11.py
@@ -0,0 +1,5 @@
+list1=[20,25,26,2,98,6]
+list2=sorted(list1)
+
+print(list1)
+print(list2)
