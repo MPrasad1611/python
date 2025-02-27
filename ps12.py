ac6c909804cd5abfb95356df3a8c5d09e042dd88
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-02-27 20:12:49 +0530

    changes

---
+++ ps12.py
@@ -0,0 +1,13 @@
+list1=[[1,2,3],[4,5,6],[7,8,9]]
+def printm(list1):
+    for i in range(len(list1)):
+      for j in range(len(list1[i])):
+          print(list1[i][j],end=" ")
+      print()  
+printm(list1)
+print("-----------")
+for i in range(len(list1)):
+    for j in range(len(list1[i])):
+          if i<=j:
+           list1[i][j],list1[j][i]=list1[j][i],list1[i][j]
+printm(list1)
