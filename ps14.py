780e2867d28077b1f4503bcce543c3b8a20a7233
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-03-01 23:32:21 +0530

    merge sort

--- ps14.py
+++ ps14.py
@@ -26,4 +26,17 @@ def merge(list1,list2):
     res.extend(list1[low1:])
     res.extend(list2[low2:])
     return res
-print(merge([1,2,3,4,5,6,7,8,9,10,344],[4,7,9,11,12,34,43,46,67,900]))
+
+def mergesort(list1,low,high):
+     if low==high:
+         return list1[low:high+1]
+     mid=(low+high)//2
+     lef=mergesort(list1,low,mid)
+     rig=mergesort(list1,mid+1,high)
+     res=merge(lef,rig)
+     list1[low:high+1]=res
+     return res
+
+list1=[44,54,12,3,56,432,7,8,9,10,344]
+mergesort(list1,0,len(list1)-1)
+print(list1)
