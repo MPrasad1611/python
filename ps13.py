15b5d542a6ff3d2e0e1acfb48d8c249da13d848e
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-03-01 12:14:23 +0530

    changes

--- ps13.py
+++ ps13.py
@@ -1,6 +1,22 @@
-def fact(n):
-    if n==1 or n==0:
-        return 1
-    return n*fact(n-1)
+# def fact(n):
+#     if n==1 or n==0:
+#         return 1
+#     return n*fact(n-1)
 
-print(fact(5))
+# print(fact(5))
+# def fin(n):
+#     if n==0 or n==1:
+#         return n
+#     return fin(n-1)+fin(n-2) 
+# print(fin(12))
+# def sofd(num):
+#     if num==0:
+#         return 0
+#     rem=num%10
+#     return rem+sofd(num//10)
+# print(sofd(87686))
+# def exponent(a,b):
+#     if b==0:
+#         return 1
+#     return a*exponent(a,b-1)
+# print(exponent(2,3))
