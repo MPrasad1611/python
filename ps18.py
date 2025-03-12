0b80271d0b28903020b12f3e2880ba9abc390343
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-03-12 21:40:24 +0530

    changes

--- ps18.py
+++ ps18.py
@@ -92,3 +92,27 @@ n=int(input("Enter the no.of stars and rows:"))
 #         else:
 #            print("  ",end='')
 #     print()
+
+# for i in range(0,n):
+#     for j in range(0,n):
+#         if i>=j:
+#          print(j+1,end=" ")
+#     print()
+# count=1
+# for i in range(0,n):
+#     for j in range(0,n):
+#         if i>=j:
+#          print(count,end=" ")
+#          count+=1
+#     print()
+for i in range(0,n):
+    for j in range(0,n):
+        if i>=j:
+          print("* ",end= '')
+    print()
+for i in range(0,n):
+    for j in range(0,n):
+        if i<=j-1:
+          print("* ",end= '')
+       
+    print()
