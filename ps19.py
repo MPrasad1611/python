07c9317c37eca71c20b0ceacfdd05b6fb4adce21
MPrasad1611 <prasadchowdary1611@gmail.com>
2025-03-16 21:35:13 +0530

    changes

---
+++ ps19.py
@@ -0,0 +1,39 @@
+# class Calculator:
+#     def __init__(self):
+#         self.result=0
+#     def add(self,a,b):
+#         self.result=a+b
+#         return self.result
+#     def sub(self,a,b):
+#         self.result=a-b
+#         return self.result
+#     def mul(self,a,b):
+#         self.result=a*b
+#         return self.result
+#     def div(self,a,b):
+#         self.result=a/b
+#         return self.result
+#     def mod(self,a,b):
+#         self.result=a%b
+#         return self.result
+#     def fact(self,a):
+#         self.result=1
+
+# cal=Calculator()
+# print(cal.add(10,20))
+# print(cal.sub(10,20))
+# print(cal.mul(10,20))
+# print(cal.div(10,20))
+# print(cal.mod(10,20))
+# print(cal.fact(5))
+
+class Human:
+    def __init__(self,name1,age1):
+        self.name=name1
+        self.age=age1
+
+human1=Human("Rahul",20)
+human2=Human("Prasad",21)
+
+print(human1.name,human1.age)
+print(human2.name,human2.age)
